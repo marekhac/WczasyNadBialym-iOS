7100dcb70866dcd7e88a2e8107336f89599a3e36
Marek Hać <fuego@o2.pl>
2020-05-05 22:38:53 +0200

    Add pre-build script for AppCenter

---
+++ WczasyNadBialym/appcenter-pre-build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo "##[warning][Pre-Build Action] - Lets do some Pre build transformations..."
+
+# Declare local script variables
+SCRIPT_ERROR=0
+
+# Define the files to manipulate
+INFO_PLIST_FILE=${APPCENTER_SOURCE_DIRECTORY}/WczasyNadBialym/WczasyNadBialym/Info.plist
+
+echo "##[warning][Pre-Build Action] - Checking if all files and environment variables are available..."
+echo "##[warning][Pre-Build Action] - APPCENTER_SOURCE_DIRECTORY: $APPCENTER_SOURCE_DIRECTORY "
+
+
+if [ -z "${APP_ID}}" ]
+then
+    echo "##[error][Pre-Build Action] - APP_ID variable needs to be defined in App Center!!!"
+    let "SCRIPT_ERROR += 1"
+    else
+    echo "##[warning][Pre-Build Action] - APP_ID variable - oK!"
+fi
+
+if [ -e "${INFO_PLIST_FILE}" ]
+then
+    echo "##[warning][Pre-Build Action] - Info.plist file found - oK!"
+else
+    echo "##[error][Pre-Build Action] - Info.plist file not found!"
+    let "SCRIPT_ERROR += 1"
+fi
+
+######################## Changes on iOS
+if [ -e "$INFO_PLIST_FILE" ]
+then
+    echo "##[command][Pre-Build Action] - Changing the App Id to: $APP_ID "
+    plutil -replace "app_id" -string "$APP_ID" $INFO_PLIST_FILE
+
+    echo "##[section][Pre-Build Action] - Info.plist File content:"
+    cat $INFO_PLIST_FILE
+    echo "##[section][Pre-Build Action] - Info.plist EOF"
+fi
