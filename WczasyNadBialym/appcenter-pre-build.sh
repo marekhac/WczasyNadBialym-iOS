c5979827f8c94222fc582fecb8317f0d7af0ffba
Marek Hać <fuego@o2.pl>
2020-05-06 09:50:50 +0200

    Add APP_CENTER_ID environment variable

--- WczasyNadBialym/appcenter-pre-build.sh
+++ WczasyNadBialym/appcenter-pre-build.sh
@@ -20,6 +20,14 @@ then
     echo "##[warning][Pre-Build Action] - APP_ID variable - oK!"
 fi
 
+if [ -z "${APP_CENTER_ID}" ]
+then
+    echo "##[error][Pre-Build Action] - APP_CENTER_ID variable needs to be defined in App Center!!!"
+    let "SCRIPT_ERROR += 1"
+    else
+    echo "##[warning][Pre-Build Action] - APP_CENTER_ID variable - oK!"
+fi
+
 if [ -e "${INFO_PLIST_FILE}" ]
 then
     echo "##[warning][Pre-Build Action] - Info.plist file found - oK!"
@@ -33,6 +41,9 @@ if [ -e "$INFO_PLIST_FILE" ]
 then
     echo "##[command][Pre-Build Action] - Changing the App Id to: $APP_ID "
     plutil -replace "app_id" -string "$APP_ID" $INFO_PLIST_FILE
+
+    echo "##[command][Pre-Build Action] - Changing the App Center Id to: $APP_CENTER_ID "
+    plutil -replace "app_center_id" -string "$APP_CENTER_ID" $INFO_PLIST_FILE
 
     echo "##[section][Pre-Build Action] - Info.plist File content:"
     cat $INFO_PLIST_FILE
